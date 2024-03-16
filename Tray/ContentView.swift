2e57d4f962f2d796ba90a2395210677e206fff1a
dankamel <danielufkamel@gmail.com>
2024-03-16 11:49:06 +0000

    begin initial tray design

--- Tray/ContentView.swift
+++ Tray/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isOption1Selected = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Color.gray.ignoresSafeArea()
+            
+            VStack {
+                Text("Beautiful Tray")
+                    .font(.system(size: 20, weight: .medium, design: .rounded))
+                    .padding(.vertical, 30)
+                
+                
+                VStack (spacing: 10) {
+                    
+                    HStack {
+                        Text ("This is option 1")
+                            .multilineTextAlignment(.leading)
+                            .onTapGesture {
+                                withAnimation (.easeInOut(duration: 0.2)){
+//                                    isOption1Selected.toggle()
+                                    isOption1Selected.toggle()
+                                }
+                            }
+                        Spacer()
+                    }
+                    .padding(10)
+                    .background(isOption1Selected ? Color.gray.opacity(0.2) : Color.clear)
+                    .mask(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    .padding(.horizontal, 40)
+                    
+                    HStack {
+                        Text ("This is option 2")
+                        Spacer()
+                    }
+                    .padding(10)
+                    .padding(.horizontal, 40)
+                    
+                    HStack {
+                        Text ("This is option 3")
+                        Spacer()
+                    }
+                    .padding(10)
+                    .padding(.horizontal, 40)
+                    
+                }
+                .font(.system(size: 18, weight: .regular, design: .rounded))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                    
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .frame(width: 325, height: 300)
+            .background(Color.white)
+            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
         }
-        .padding()
+
     }
 }
 
