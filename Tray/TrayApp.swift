9be372925931bc07cfec0844ede06c12cc09f0ca
dankamel <danielufkamel@gmail.com>
2024-03-16 11:43:27 +0000

    Initial Commit

---
+++ Tray/TrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrayApp.swift
+//  Tray
+//
+//  Created by Daniel Kamel on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
